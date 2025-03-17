4027846aa4372040a6ea9550543aeaf292b52a91
nast <eha20082@gmail.com>
2025-03-17 08:33:36 +0200

    Update wordd1.py

--- wordd1/wordd1.py
+++ wordd1/wordd1.py
@@ -1,35 +1,53 @@
+from colorama import Fore, Back, Style
 import random
 
 words = ["Looma", "Ratas", "Käige", "Sõber", "Laine", "Pagar", "Roosa", "Tunne", "Kalur", "Hinge"]
 randomword = random.choice(words)
 length = len(randomword)
+
 print('''
          RULES!
 You have to guess the word.
 You must enter letters from the alphabet.
 You have 6 tries!
 ''')
+
 print(f"The word has {length} letters. Try to guess it!")
+
 attempts = 6  
-while attempts > 0:
-    try:
-        user = input("Your guess: ")
 
-        if len(user) > length:
-            print("Your word is too long!")
-        elif len(user) < length:
-            print("Your word is too short!")
-        elif user.isalpha() == False:
-            print("Please enter only letters!")
-        elif user == randomword:
-            print("Congratulations! You guessed the word!")
-            break
+for j in range(6):
+    result = ""
+    print(f"Katse {j+1}/6")
+
+    while 1:
+        try:
+            user = str(input("Your guess: ")).lower()
+
+            if len(user) > length:
+                print("Your word is too long! Please enter a word with the correct number of letters.")
+            elif len(user) < length:
+                print("Your word is too short! Please enter a word with the correct number of letters.")
+            elif user.isalpha() == False:
+                print("Please enter only letters!")
+            elif len(user) == length:
+                break
+        except:
+            print("Please enter a valid word consisting only of letters!")
+
+    for i in range(length):
+        if user[i] == randomword[i].lower():
+            result += Fore.GREEN + user[i] + Style.RESET_ALL
+        elif user[i] in randomword.lower() and user[i] != randomword[i].lower():
+            result += Fore.YELLOW + user[i] + Style.RESET_ALL
         else:
-            attempts -= 1
-            print(f"Wrong guess! You have {attempts} attempts left.")
+            result += "-"
 
-        if attempts == 0:
-            print(f"Game over! The word was: {randomword}")
+    result += "\n"
+    print(result)
 
-    except ValueError:
-        print("Error")
+    if user == randomword.lower():
+        print(f"Palju õnne, te võitsite! Mõeldud sõna on: {randomword}")
+        break
+else:
+    print(f"Te kaotasite! Mõeldud sõna on: {randomword}")
